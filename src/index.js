64606cde5a0e99e6576a97640ca76762cfa572e6
Dilyara Arslanova <risharsivil@gmail.com>
2022-07-28 16:10:44 +0300

    55

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+    for (i = 0; i <= str.length - 1; i++){
+        for (j = 0; j <= bracketsConfig.length - 1; j++){
+           if (str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]){
+                if (stack[stack.length -1] === str[i]){
+                    stack.pop(str[i])
+                } else stack.push(str[i])
+            } else if ((str[i] === bracketsConfig[j][0])){
+                stack.push(str[i])
+            } else if (str[i] === bracketsConfig[j][1]){
+                if (stack[stack.length -1] === bracketsConfig[j][0]){
+                    stack.pop(str[i])
+                } else return false
+            } 
+        }
+    }
+    return stack.length === 0
 }
